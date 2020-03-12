4e439f4d21101287a6d815b7611af7bce3a7caed
s4178jm <60681747+s4178jm@users.noreply.github.com>
2020-03-12 14:14:58 -0700

    Add files via upload

---
+++ app.js
@@ -0,0 +1,31 @@
+
+
+/* const scrollFunc = () => {
+  let y = window.scrollY;
+  
+  if (y > 0) {
+    scrollToTopButton.className = "top-link show";
+  } else {
+    scrollToTopButton.className = "top-link hide";
+  }
+}; */
+
+window.addEventListener("scroll", scrollFunc);
+
+
+const scrollToTopButton = document.getElementById('js-top');
+
+const scrollToTop = () => {
+
+  const c = document.documentElement.scrollTop || document.body.scrollTop;
+  if (c > 0) {
+    window.requestAnimationFrame(scrollToTop);
+    window.scrollTo(0, c - c / 10);
+  }
+};
+
+scrollToTopButton.onclick = function(e) {
+  e.preventDefault(); // could cause page to refresh 
+  scrollToTop();
+}
+
