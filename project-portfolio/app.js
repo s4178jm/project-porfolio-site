8c2dc216f4972a1e27b0b831dc0645949aae56bf
s4178jm <60681747+s4178jm@users.noreply.github.com>
2020-03-12 17:25:13 -0700

    Add files via upload

--- project-portfolio/app.js
+++ project-portfolio/app.js
@@ -1,78 +1,31 @@
 
-let url= "https://docs.google.com/spreadsheets/d/1kkg45Z5bjFHx27UMlTrIGjzm12GDoqMksyqX8CPPFh0/edit#gid=0"
 
-let id = '1Ox49O2OQueIyS5QUvW0Jp1bW7SDUO1Rge2cf-b_2LfU'
+/* const scrollFunc = () => {
+  let y = window.scrollY;
+  
+  if (y > 0) {
+    scrollToTopButton.className = "top-link show";
+  } else {
+    scrollToTopButton.className = "top-link hide";
+  }
+}; */
 
-// let source = `https://spreadsheets.google.com/feeds/list/1Ox49O2OQueIyS5QUvW0Jp1bW7SDUO1Rge2cf-b_2LfU/od6/public/values?alt=json`
-let source = `https://spreadsheets.google.com/feeds/list/${id}/od6/public/values?alt=json`
+window.addEventListener("scroll", scrollFunc);
 
-// ES6 - fetch
 
-// fetch queries the url provided and requests that the data be sent to it 
-// it receives JSON => JS Object Notation
-// we use .json() to parse the data from "{}" to an full blown {}
-fetch(source)
-  .then( response => response.json() ) // this parses the data from string back into an object
-  .then( data =>  {
-      console.log('data', data)
-      // data.feed.entry is the array that stores our projects
-      // the projects are stored as objects
-      let projects = data.feed.entry.map( project => {
-        // console.log('project', project.gsx$title.$t)
-        return {
-          title: project.gsx$title.$t,
-          image: project.gsx$image.$t,
-          description: project.gsx$description.$t,
-          url: project.gsx$url.$t
-        }
-      })
-      app(projects)
-  }) // this provides us access to the parse data
-  // .catch( err => console.log('err', err))
+const scrollToTopButton = document.getElementById('js-top');
 
-  let $sectionContainer = $('.projects')
+const scrollToTop = () => {
 
-function app(projects) {
-   projects.forEach(function(element) {
-     let $cardContainer = $('<div>').addClass('card')
-     let $imgContainer = $('<div>').addClass('cardBackground')
-     let $infoBody = $('<div>').addClass('cardBody')
-     $cardContainer.append($imgContainer)
-     $cardContainer.append($infoBody)
-     let $cardHead = $('<h4>').addClass('card-title')
-     let $cardParagraph = $('<p>').addClass('card-text')
-     $cardHead.append(element['title'])
-     $cardParagraph.append(element['description'])
-     $infoBody.append($cardHead)
-     $infoBody.append($cardParagraph)
-     $imgContainer.css('background-image', 'url("'+ element['image'] +'")')
-     let $buttonPrimary = $('<button>').addClass('projectBtn').html('See the app')
-     $infoBody.append($buttonPrimary)
-    $sectionContainer.append($cardContainer)
-    $buttonPrimary.on('click', function() {
-        window.open(element['url'])
-        return false
-    })
-   })
-}
-
-/*const $section = $('section');
-const $div = $('<div>');
-
-$section.append($div);
-
-const createTable = () => {
-    cardArray.forEach( iconData => {
-        let $li = $(`<div class="card">
-                    <img src="${iconData.image}>
-                    <div class="card-body">
-                    <h4 class="card-title">${iconData.title}</h4>
-                     <p class="card-text">${iconData.description}</p>
-                    <a class="btn btn-primary live-link" href="${iconData.Link}" target="_blank">See the app</a>
-                    </div>
-                     </div>`)
-        $div.append($li);
-    })
+  const c = document.documentElement.scrollTop || document.body.scrollTop;
+  if (c > 0) {
+    window.requestAnimationFrame(scrollToTop);
+    window.scrollTo(0, c - c / 10);
+  }
 };
 
-createTable(); */
+scrollToTopButton.onclick = function(e) {
+  e.preventDefault(); // could cause page to refresh 
+  scrollToTop();
+}
+
